edb1ff96bcd71d45dd9f45e85712af2521f58909
Daniel Gillespie <danielg@umd.edu>
2013-08-20 22:19:58 -0400

    Initial Rails Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShellWeb::Application.config.secret_key_base = '7a50ababd20a457a3d2630032decafb39d16fa4aaa200c3ac2086f07409825da130ed2a7e7490a870a9203c467b5667cbf5db5b1744585cb02d639b67da60d80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShellWeb::Application.config.session_store :cookie_store, key: '_ShellWeb_session'

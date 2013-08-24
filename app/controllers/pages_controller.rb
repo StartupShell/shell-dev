17dd981aede38cdf09dba5d41a7be6a9073051c9
Daniel Gillespie <danielg@umd.edu>
2013-08-23 21:40:00 -0400

    Added caching to calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,7 @@ class PagesController < ApplicationController
 
 	layout 'frontend'
 	def index 
-		require 'open-uri'
-		require 'json'
-		feed = "7qvrobfs0js5799ebugodgc5go%40group.calendar.google.com"
-		results = "4"
-		url = "http://www.google.com/calendar/feeds/"+feed+"/public/full?alt=json&orderby=starttime&max-results="+results+"&singleevents=true&sortorder=ascending&futureevents=true"
-		doc = open(url)
-		jsonObj = JSON.load(doc)
-		@events = jsonObj['feed']['entry']
+		
 	end
 
 end

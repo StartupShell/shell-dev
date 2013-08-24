59113ef7f3f836b68f5506b38090c432cdc0463d
Daniel Gillespie <danielg@umd.edu>
2013-08-23 21:33:54 -0400

    no images but otherwise almost ready

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
 
 	layout 'frontend'
-	def index
+	def index 
 		require 'open-uri'
 		require 'json'
 		feed = "7qvrobfs0js5799ebugodgc5go%40group.calendar.google.com"
-		results = "5"
+		results = "4"
 		url = "http://www.google.com/calendar/feeds/"+feed+"/public/full?alt=json&orderby=starttime&max-results="+results+"&singleevents=true&sortorder=ascending&futureevents=true"
 		doc = open(url)
 		jsonObj = JSON.load(doc)

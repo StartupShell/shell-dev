e835405a57e68c27f64ef70e8015522462053ef4
Daniel Gillespie <danielg@umd.edu>
2013-08-27 03:22:24 -0400

    added javascript magic and some css

--- app/assets/javascripts/scrollScripts.js
+++ app/assets/javascripts/scrollScripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
   //nav-active
   smoothScroll();
-  //scrollDisplay();
+  scrollDisplay();
 });
 
 function smoothScroll() {
@@ -21,5 +21,43 @@ function smoothScroll() {
       }
     }
   });
+}
+
+function scrollDisplay() {
+
+  var header = $("#header"),
+      headerHeight = header.outerHeight()+15,
+      // All list items
+      headerItems = header.find("a"),
+      // Anchors corresponding to menu items
+      scrollItems = headerItems.map(function(){
+        var input = $(this).attr("href").replace("/","");
+        var item = $(input);
+        if (item.length) {return item; }
+      });
+
+  // Bind to scroll
+  $(window).scroll(function(){
+     // Get container scroll position
+     var fromTop = $(this).scrollTop()+headerHeight;
+     // Get id of current scroll item
+    if (fromTop < 160) {
+      $("#nav-apply").addClass("nav-emphasis");
+      $(headerItems).removeClass("nav-active");
+    } else  {
+      $("#nav-apply").removeClass("nav-emphasis");
+      var cur = scrollItems.map(function(){
+       if ($(this).offset().top < fromTop)
+         return this;
+     });
+     // Get the id of the current element
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+     // Set/remove active class
+     $(headerItems).removeClass("nav-active");
+     $("#"+id+"I").addClass("nav-active");
+    }
+     
+   });
 }
 

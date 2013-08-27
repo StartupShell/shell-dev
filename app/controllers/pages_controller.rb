e319f4673f5f02a0d6751c1bb31e6dc2a8c44330
Daniel Gillespie <danielg@umd.edu>
2013-08-26 23:43:14 -0400

    New CSS/ added startup config to controller /Added blissic to startups

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,19 @@
 class PagesController < ApplicationController
-
 	layout 'frontend'
 	def index 
-		
+		startupFiles = [["http://512technology.com", "logo-512.png", "512 Technology"],
+            ["", "logo-arcadeglass.png", "Arcade Glass"], 
+            ["http://shopevlove.com", "logo-evlove.png", "Evlove"],
+            ["http://foodrecoverynetwork.org", "logo-frn.png", "Food Recovery Network"],
+            ["http://jhilmd.com", "logo-jhilmediadesign.png", "JHil Media Design"],
+            ["http://paralleltracks.com", "logo-paralleltracks.png", "Parallel Tracks"],
+            ["", "logo-sweetbuds.png", "Sweet Buds"],
+            ["http://www.blissic.com", "logo-blissic.png", "Blissic"],
+            ["http://ventureboard.co", "logo-ventureboard.png", "VentureBoard"]]
+         @startups = startupFiles.shuffle
 	end
 
 	def apply
-
 	end
 
 end
